b96e67a54343d864c489458545486099559e9dd0
tubagus-danny <danny.putranto04@gmail.com>
2019-10-15 14:18:04 +0700

    tugas akhir

---
+++ README.md
@@ -0,0 +1,11 @@
+# GoCommerce
+Simple React Native eCommerce mobile application.
+
+# Running the app
+1- `npm install` 
+
+2- `npm install -g json-server`
+
+3- `json-server server.json`
+
+5- `react-native run-ios` or `react-native run-android`
---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import { Platform } from 'react-native';
+
+const api = axios.create({
+  baseURL:
+    // IF RUNNING WITHOUT GENYMOTION, USE 10.0.2.2
+    Platform.OS === 'android' ? 'http://10.0.2.2:3000' : 'http://localhost:3000'
+});
+
+export default api;
